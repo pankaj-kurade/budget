2bad0dd93644ea054ec29b7a1d82e9bc216f8323
pankaj-kurade <pankajkurade316@gmail.com>
2024-03-10 21:50:42 +0530

    10

--- src/app/budget-planner/budget-planner.module.ts
+++ src/app/budget-planner/budget-planner.module.ts
@@ -4,12 +4,14 @@ import { BudgetPlannerRoutingModule } from './budget-planner-routing.module';
 import { LoginComponent } from './login/login.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { Validators } from '@angular/forms';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+
 @NgModule({
   declarations: [
     LoginComponent
   ],
   imports: [
-    CommonModule,ReactiveFormsModule,
+    CommonModule,ReactiveFormsModule,MatSnackBarModule,
     BudgetPlannerRoutingModule
   ]
 })
--- src/app/budget-planner/login/login.component.ts
+++ src/app/budget-planner/login/login.component.ts
@@ -1,21 +1,27 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import {MatSnackBar} from '@angular/material/snack-bar';
+
+
+// import {MatSnackBar} from '@angular/material/snack-bar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
+  
 })
 export class LoginComponent {
 
-onsubmit() {
-throw new Error('Method not implemented.');
-}
+
 loginForm:any;
 registerForm:any;
 activeForm:'login'|'register' = 'login';
-constructor(private fb:FormBuilder){}
+constructor(private fb:FormBuilder,private _snackBar: MatSnackBar){}
 
 toggle(form :'login'|'register') {
 
@@ -35,4 +41,18 @@ email:['',[Validators.required,Validators.email]]
 })
 
 }
+
+onsubmit() {
+  if(this.loginForm.valid){
+    console.log(this.loginForm.value);
+    
+  }
+  else{
+    console.log("he");
+
+    this._snackBar.open("message");
+
+  }
+  
+  }
 }
