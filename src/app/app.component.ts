3ef8ab60ce0c440fbceefe9d9b84e1af79778ed8
Pankaj <pankajkurade316@gmail.com.com>
2024-04-22 19:43:34 +0530

    ngonoit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'budget';
+constructor(private router:Router){}
+  ngOnInit(){
+this.router.navigate(['./budget-planner/login'])
+  }
 }
+
+git config --global user.email "pankajkurade316@gmail.com.com"
+git config --global user.name "Pankaj"
